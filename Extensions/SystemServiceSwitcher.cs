3998dfe0f9611830f04b0c82805090e9771ccedb
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2017-03-02 21:31:44 +0300

    Init project 2015

---
+++ Extensions/SystemServiceSwitcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace KpdApps.Common.MsCrm2015.Extensions
+{
+	public sealed class SystemServiceSwitcher : IDisposable
+	{
+		private IOrganizationService Service { get; }
+
+		private Guid UserCallerId { get; }
+
+		private static readonly Dictionary<Guid, Guid> OrgSystemUserIds = new Dictionary<Guid, Guid>();
+
+		protected Guid SystemUser
+		{
+			get
+			{
+				var orgId = ((WhoAmIResponse)Service.Execute(new WhoAmIRequest())).OrganizationId;
+
+				if (OrgSystemUserIds.ContainsKey(orgId))
+					return OrgSystemUserIds[orgId];
+
+				var query = new QueryExpression
+				{
+					EntityName = "systemuser",
+					NoLock = true,
+					Criteria =
+					{
+						Conditions =
+						{
+							new ConditionExpression("fullname", ConditionOperator.Equal, "SYSTEM")
+						}
+					}
+				};
+
+				EntityCollection users = Service.RetrieveMultiple(query);
+				if (users.Entities.Count > 0)
+					OrgSystemUserIds.Add(orgId, users[0].Id);
+				else throw new Exception("Пользователь SYSTEM не найден.");
+
+				return OrgSystemUserIds[orgId];
+			}
+		}
+
+		public SystemServiceSwitcher(IOrganizationService service)
+		{
+			Service = service;
+			UserCallerId = ((OrganizationServiceProxy)Service).CallerId;
+			((OrganizationServiceProxy)Service).CallerId = SystemUser;
+		}
+
+		public void Dispose()
+		{
+			if (Service != null && ((OrganizationServiceProxy)Service).CallerId != UserCallerId)
+				((OrganizationServiceProxy)Service).CallerId = UserCallerId;
+		}
+	}
+}
