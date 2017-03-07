a5810b17902c96ca85aa73aa74d0517d2f74abfd
Dmitriy Pelevin <dmitry@pelevin.pro>
2017-03-07 19:51:05 +0300

    Add BasePluginAction

---
+++ BasePluginAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KpdApps.Common.MsCrm2015
+{
+    public class BasePluginAction
+    {
+        public PluginState State { get; set; }
+
+        public BasePluginAction(PluginState state)
+        {
+            State = state;
+        }
+
+        public virtual void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
