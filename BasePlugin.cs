d883bb1d79a4bf97af92168f4f22e86fdbabf54d
Dmitriy Pelevin <dmitry@pelevin.pro>
2017-03-07 19:57:33 +0300

    BasePlugin trace execution errors

--- BasePlugin.cs
+++ BasePlugin.cs
@@ -1,53 +1,54 @@
 ﻿using System;
+using KpdApps.Common.MsCrm2015.Extensions;
 using Microsoft.Xrm.Sdk;
 
 namespace KpdApps.Common.MsCrm2015
 {
-	public abstract class BasePlugin : IPlugin
-	{
-		public readonly string UnsecureConfiguration;
+    public abstract class BasePlugin : IPlugin
+    {
+        public readonly string UnsecureConfiguration;
 
-		public readonly string SecureConfiguration;
+        public readonly string SecureConfiguration;
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="unsecureConfiguration"></param>
-		/// <param name="secureConfiguration"></param>
-		public BasePlugin(string unsecureConfiguration, string secureConfiguration)
-		{
-			UnsecureConfiguration = unsecureConfiguration;
-			SecureConfiguration = secureConfiguration;
-		}
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="unsecureConfiguration"></param>
+        /// <param name="secureConfiguration"></param>
+        public BasePlugin(string unsecureConfiguration, string secureConfiguration)
+        {
+            UnsecureConfiguration = unsecureConfiguration;
+            SecureConfiguration = secureConfiguration;
+        }
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		public BasePlugin()
-		{
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BasePlugin()
+        {
 
-		}
+        }
 
-		public void Execute(IServiceProvider serviceProvider)
-		{
-			PluginState state = CreatePluginState(serviceProvider);
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            PluginState state = CreatePluginState(serviceProvider);
 
-			try
-			{
-				ExecuteInternal(state);
-			}
-			catch (Exception)
-			{
-				//TODO: TraceError
-				throw;
-			}
-		}
+            try
+            {
+                ExecuteInternal(state);
+            }
+            catch (Exception ex)
+            {
+                state?.TraceError(ex);
+                throw;
+            }
+        }
 
-		public abstract void ExecuteInternal(PluginState state);
+        public abstract void ExecuteInternal(PluginState state);
 
-		public virtual PluginState CreatePluginState(IServiceProvider serviceProvider)
-		{
-			return new PluginState(serviceProvider);
-		}
-	}
+        public virtual PluginState CreatePluginState(IServiceProvider serviceProvider)
+        {
+            return new PluginState(serviceProvider);
+        }
+    }
 }
