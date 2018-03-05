425de64132f1e80c4195f68717983fec63df105a
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2018-03-05 08:11:48 +0300

    SetStateActive, SetStateInactive for Entity and EntityReference

--- Extensions/OrganizationServiceExtensions.cs
+++ Extensions/OrganizationServiceExtensions.cs
@@ -7,46 +7,46 @@ using Microsoft.Xrm.Sdk.Query;
 
 namespace KpdApps.Common.MsCrm2015.Extensions
 {
-	/// <summary>
-	/// Extensions for <see cref="IOrganizationService"/>.
-	/// </summary>
-	public static class OrganizationServiceExtensions
-	{
-		/// <summary>
-		/// Create entity.
-		/// </summary>
-		/// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
-		/// <param name="entity">Entity to create.</param>
-		public static Guid CreateEntity(this IOrganizationService organizationService, Entity entity)
-		{
-			if (entity == null || !entity.Attributes.Any())
-				return Guid.Empty;
+    /// <summary>
+    /// Extensions for <see cref="IOrganizationService"/>.
+    /// </summary>
+    public static class OrganizationServiceExtensions
+    {
+        /// <summary>
+        /// Create entity.
+        /// </summary>
+        /// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
+        /// <param name="entity">Entity to create.</param>
+        public static Guid CreateEntity(this IOrganizationService organizationService, Entity entity)
+        {
+            if (entity == null || !entity.Attributes.Any())
+                return Guid.Empty;
 
-			return organizationService.Create(entity);
-		}
+            return organizationService.Create(entity);
+        }
 
-		/// <summary>
-		/// Replace for regular Retrieve, return <see cref="Entity"/> with all collumns.
-		/// </summary>
-		/// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
-		/// <param name="entityName">Entity name.</param>
-		/// <param name="entityId">Entity identifier.</param>
-		/// <returns><see cref="Entity"/></returns>
-		public static Entity Retrieve(this IOrganizationService organizationService, string entityName, Guid entityId)
-		{
-			return organizationService.Retrieve(entityName, entityId, new ColumnSet(true));
-		}
+        /// <summary>
+        /// Replace for regular Retrieve, return <see cref="Entity"/> with all collumns.
+        /// </summary>
+        /// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
+        /// <param name="entityName">Entity name.</param>
+        /// <param name="entityId">Entity identifier.</param>
+        /// <returns><see cref="Entity"/></returns>
+        public static Entity Retrieve(this IOrganizationService organizationService, string entityName, Guid entityId)
+        {
+            return organizationService.Retrieve(entityName, entityId, new ColumnSet(true));
+        }
 
-		/// <summary>
-		/// Replace for regular Retrieve, return <see cref="Entity"/> with all collumns.
-		/// </summary>
-		/// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
-		/// <param name="entityReference"><see cref="EntityReference"/></param>
-		/// <returns><see cref="Entity"/></returns>
-		public static Entity Retrieve(this IOrganizationService organizationService, EntityReference entityReference)
-		{
-			return organizationService.Retrieve(entityReference.LogicalName, entityReference.Id, new ColumnSet(true));
-		}
+        /// <summary>
+        /// Replace for regular Retrieve, return <see cref="Entity"/> with all collumns.
+        /// </summary>
+        /// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
+        /// <param name="entityReference"><see cref="EntityReference"/></param>
+        /// <returns><see cref="Entity"/></returns>
+        public static Entity Retrieve(this IOrganizationService organizationService, EntityReference entityReference)
+        {
+            return organizationService.Retrieve(entityReference.LogicalName, entityReference.Id, new ColumnSet(true));
+        }
 
         /// <summary>
         /// Replace for regular Retrieve, return <see cref="Entity"/> with all collumns.
@@ -68,30 +68,30 @@ namespace KpdApps.Common.MsCrm2015.Extensions
         /// <param name="referencingAttributeName">Name of attribute of reference to retrieve.</param>
         /// <returns><see cref="Entity"/></returns>
         public static Entity Retrieve(this IOrganizationService organizationService, Entity source, string referencingAttributeName)
-		{
-			var reference = source[referencingAttributeName] as EntityReference;
-			if (reference != null)
-				return Retrieve(organizationService, reference);
+        {
+            var reference = source[referencingAttributeName] as EntityReference;
+            if (reference != null)
+                return Retrieve(organizationService, reference);
 
-			return null;
-		}
+            return null;
+        }
 
-		/// <summary>
-		/// Replace for regular Retrieve, return <see cref="Entity"/> with all specified collumns.
-		/// </summary>
-		/// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
-		/// <param name="entityName">Entity name.</param>
-		/// <param name="entityId">Entity identifier.</param>
-		/// <param name="attrs">Attributes to select.</param>
-		/// <returns><see cref="Entity"/></returns>
-		public static Entity Retrieve(this IOrganizationService organizationService, string entityName, Guid entityId, params string[] attrs)
-		{
-			if (!attrs.Any())
-				return organizationService.Retrieve(entityName, entityId, new ColumnSet(true));
+        /// <summary>
+        /// Replace for regular Retrieve, return <see cref="Entity"/> with all specified collumns.
+        /// </summary>
+        /// <param name="organizationService"><see cref="IOrganizationService"/>.</param>
+        /// <param name="entityName">Entity name.</param>
+        /// <param name="entityId">Entity identifier.</param>
+        /// <param name="attrs">Attributes to select.</param>
+        /// <returns><see cref="Entity"/></returns>
+        public static Entity Retrieve(this IOrganizationService organizationService, string entityName, Guid entityId, params string[] attrs)
+        {
+            if (!attrs.Any())
+                return organizationService.Retrieve(entityName, entityId, new ColumnSet(true));
 
-			ColumnSet set = new ColumnSet(attrs);
-			return organizationService.Retrieve(entityName, entityId, set);
-		}
+            ColumnSet set = new ColumnSet(attrs);
+            return organizationService.Retrieve(entityName, entityId, set);
+        }
 
         /// <summary>
         /// Replace for regular Retrieve, return <see cref="Entity"/> with all specified collumns.
@@ -106,73 +106,93 @@ namespace KpdApps.Common.MsCrm2015.Extensions
         }
 
         public static SystemServiceSwitcher SwitchToSystem(this IOrganizationService service)
-		{
-			return new SystemServiceSwitcher(service);
-		}
+        {
+            return new SystemServiceSwitcher(service);
+        }
 
-		public static ExecuteWorkflowResponse ExecuteWorkflowWithRetry(this IOrganizationService service, Guid entityId, Guid workflowId, int retryTimes = 1)
-		{
-			ExecuteWorkflowRequest request = new ExecuteWorkflowRequest
-			{
-				EntityId = entityId,
-				WorkflowId = workflowId
-			};
-			return ExecuteWorkflowWithRetry(service, request, retryTimes);
-		}
+        public static ExecuteWorkflowResponse ExecuteWorkflowWithRetry(this IOrganizationService service, Guid entityId, Guid workflowId, int retryTimes = 1)
+        {
+            ExecuteWorkflowRequest request = new ExecuteWorkflowRequest
+            {
+                EntityId = entityId,
+                WorkflowId = workflowId
+            };
+            return ExecuteWorkflowWithRetry(service, request, retryTimes);
+        }
 
-		public static ExecuteWorkflowResponse ExecuteWorkflowWithRetry(this IOrganizationService service, ExecuteWorkflowRequest request, int retryTimes)
-		{
-			ExecuteWorkflowResponse response = new ExecuteWorkflowResponse();
-			int i = 0;
-			while (i < retryTimes)
-			{
-				try
-				{
-					response = (ExecuteWorkflowResponse)service.Execute(request);
-					break;
-				}
-				catch (SqlException)
-				{
-					i++;
-					System.Threading.Thread.Sleep(i * 1000);
-				}
-			}
+        public static ExecuteWorkflowResponse ExecuteWorkflowWithRetry(this IOrganizationService service, ExecuteWorkflowRequest request, int retryTimes)
+        {
+            ExecuteWorkflowResponse response = new ExecuteWorkflowResponse();
+            int i = 0;
+            while (i < retryTimes)
+            {
+                try
+                {
+                    response = (ExecuteWorkflowResponse)service.Execute(request);
+                    break;
+                }
+                catch (SqlException)
+                {
+                    i++;
+                    System.Threading.Thread.Sleep(i * 1000);
+                }
+            }
 
-			if (i >= retryTimes)
-				throw new Exception($"ExecuteWorkflowWithRetry WorkflowId({request.WorkflowId}): исчерпан лимит попыток.");
+            if (i >= retryTimes)
+                throw new Exception($"ExecuteWorkflowWithRetry WorkflowId({request.WorkflowId}): исчерпан лимит попыток.");
 
-			return response;
-		}
+            return response;
+        }
 
-		public static void SetStateActive(this IOrganizationService service, string entityName, Guid id)
-		{
-			SetStateDynamic(service, entityName, id, 0, 1);
-		}
+        public static void SetStateActive(this IOrganizationService service, Entity entity)
+        {
+            SetStateActive(service, entity.LogicalName, entity.Id);
+        }
 
-		public static void SetStateInactive(this IOrganizationService service, string entityName, Guid id)
-		{
-			SetStateDynamic(service, entityName, id, 1, 2);
-		}
+        public static void SetStateActive(this IOrganizationService service, EntityReference entityReference)
+        {
+            SetStateActive(service, entityReference.LogicalName, entityReference.Id);
+        }
 
-		/// <summary>
-		/// Execute <see cref="SetStateRequest"/> to set entity state and status codes.
-		/// </summary>
-		/// <param name="service"><see cref="IOrganizationService"/></param>
-		/// <param name="entityName">Logical name of entity.</param>
-		/// <param name="id">Entity identifier.</param>
-		/// <param name="stateCode">State code</param>
-		/// <param name="statusCode">Status code</param>
-		public static void SetStateDynamic(this IOrganizationService service, string entityName, Guid id, int stateCode, int statusCode)
-		{
-			SetStateRequest request = new SetStateRequest
-			{
-				EntityMoniker = new EntityReference(entityName, id),
-				State = new OptionSetValue(stateCode),
-				Status = new OptionSetValue(statusCode)
-			};
+        public static void SetStateActive(this IOrganizationService service, string entityName, Guid id)
+        {
+            SetStateDynamic(service, entityName, id, 0, 1);
+        }
 
-			service.Execute(request);
-		}
+        public static void SetStateInactive(this IOrganizationService service, Entity entity)
+        {
+            SetStateInactive(service, entity.LogicalName, entity.Id);
+        }
+
+        public static void SetStateInactive(this IOrganizationService service, EntityReference entityReference)
+        {
+            SetStateInactive(service, entityReference.LogicalName, entityReference.Id);
+        }
+
+        public static void SetStateInactive(this IOrganizationService service, string entityName, Guid id)
+        {
+            SetStateDynamic(service, entityName, id, 1, 2);
+        }
+
+        /// <summary>
+        /// Execute <see cref="SetStateRequest"/> to set entity state and status codes.
+        /// </summary>
+        /// <param name="service"><see cref="IOrganizationService"/></param>
+        /// <param name="entityName">Logical name of entity.</param>
+        /// <param name="id">Entity identifier.</param>
+        /// <param name="stateCode">State code</param>
+        /// <param name="statusCode">Status code</param>
+        public static void SetStateDynamic(this IOrganizationService service, string entityName, Guid id, int stateCode, int statusCode)
+        {
+            SetStateRequest request = new SetStateRequest
+            {
+                EntityMoniker = new EntityReference(entityName, id),
+                State = new OptionSetValue(stateCode),
+                Status = new OptionSetValue(statusCode)
+            };
+
+            service.Execute(request);
+        }
 
         /// <summary>
         /// Execute <see cref="SetStateRequest"/> to set entity state and status codes.
